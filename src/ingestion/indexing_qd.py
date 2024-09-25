312954134ad6fd2819266972f5ec968316e72936
Paolo Racca <paolo.racca@intesasanpaolo.com>
2024-09-25 12:41:09 +0200

    Added config file + adapted vdb pipeline

--- src/ingestion/indexing_qd.py
+++ src/ingestion/indexing_qd.py
@@ -11,11 +11,11 @@ from ingestion.vdb_wrapper import LoadInVdb
 
 # Function to read the HTML file and convert it to markdown using markdown-it-py
 def convert_html_to_markdown(html_file):
-    with open(html_file, 'r', encoding='utf-8') as file:
+    with open(html_file, "r", encoding="utf-8") as file:
         html_content = file.read()
 
     # Parse the HTML using BeautifulSoup to clean it
-    soup = BeautifulSoup(html_content, 'html.parser')
+    soup = BeautifulSoup(html_content, "html.parser")
 
     # Convert HTML to Markdown
     markdown_content = markdownify.markdownify(str(soup), heading_style="ATX")
@@ -27,29 +27,14 @@ def convert_html_to_markdown(html_file):
 def chunk_text(text, chunk_size=300):
     # TODO change asap considering the splitting with \n and a min and max size of the chunk
     words = text.split()
-    chunks = [' '.join(words[i:i + chunk_size]) for i in range(0, len(words), chunk_size)]
+    chunks = [
+        " ".join(words[i : i + chunk_size]) for i in range(0, len(words), chunk_size)
+    ]
     return chunks
 
 
-if __name__ == "__main__":
-    from qdrant_client.qdrant_client import QdrantClient
-
-    # TODO: move to config
-    PATH_TO_DB = r'c:\profili\u411319\Documents\tmp\qdrant_1'
-    client = QdrantClient(path=PATH_TO_DB)
-
-    # TODO: create main_indexing with input:
-    # html path dirname
-    COLLECTION_NAME = 'articles'
-    COLL_FRESH_START = True
-
-    # TODO: move this path to config file, relative to project root
-    # Set the path of the HTML file
-    project_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    html_folder_path = os.path.join(project_root, 'data', 'docs')
-
-    loader = LoadInVdb(client=client, coll_name=COLLECTION_NAME)
-    loader.setup_collection(is_fresh_start=COLL_FRESH_START)
+def main_indexing(loader: LoadInVdb, is_fresh_start: bool, html_folder_path: str):
+    loader.setup_collection(is_fresh_start=is_fresh_start)
 
     for f in os.listdir(html_folder_path):
         html_file_path = os.path.join(html_folder_path, f)
@@ -64,6 +49,32 @@ if __name__ == "__main__":
         if len(chunks) > 0:
             # TODO: more informative payloads need to be computed during ingestion phase!
             loader.add_to_collection(
-                dense_vectors=[compute_dense_vector(query_text=chunk) for chunk in chunks],
-                sparse_vectors=[models.SparseVector(**compute_sparse_vector(query_text=chunk)) for chunk in chunks],
-                payloads=[{'text': chunk} for chunk in chunks])
+                dense_vectors=[
+                    compute_dense_vector(query_text=chunk) for chunk in chunks
+                ],
+                sparse_vectors=[
+                    models.SparseVector(**compute_sparse_vector(query_text=chunk))
+                    for chunk in chunks
+                ],
+                payloads=[{"text": chunk} for chunk in chunks],
+            )
+
+
+if __name__ == "__main__":
+    from qdrant_client.qdrant_client import QdrantClient
+    from utils.read_config import get_config_from_path
+
+    dct_config = get_config_from_path("config.yaml")
+    client = QdrantClient(path=dct_config["VECTOR_DB"]["PATH_TO_FOLDER"])
+
+    # TODO: create main_indexing with input:
+    COLLECTION_NAME = dct_config["VECTOR_DB"]["COLLECTION_NAME"]
+    COLL_FRESH_START = dct_config["VECTOR_DB"]["COLL_FRESH_START"]
+    html_folder_path = dct_config["INPUT_DATA"]["PATH_TO_FOLDER"]
+    loader = LoadInVdb(client=client, coll_name=COLLECTION_NAME)
+
+    main_indexing(
+        loader=loader,
+        is_fresh_start=COLL_FRESH_START,
+        html_folder_path=html_folder_path,
+    )
