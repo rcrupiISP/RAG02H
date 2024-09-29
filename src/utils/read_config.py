9d0a78ea260b00805f990139c179ba9cea25fbc0
rcrupi <crupi.riccardo@spes.uniud.it>
2024-09-29 21:40:25 +0200

    Add requirements from qdrant. Add configuration working for PyCharm.

--- src/utils/read_config.py
+++ src/utils/read_config.py
@@ -5,7 +5,17 @@ import os
 
 
 def get_config_from_path(file_name: str) -> dict:
+
+    if os.environ.get("MY_HOME") is None:
+        tmp_path = os.getcwd()
+        rag_index = tmp_path.find('RAG02H')
+        root_path = tmp_path[:rag_index + len('RAG02H')]
+        os.environ["MY_HOME"] = root_path.replace('\\', '/')
+    if os.environ.get("APP_CONF_DIR") is None:
+        os.environ["APP_CONF_DIR"] = os.path.join(os.environ["MY_HOME"], "src\\config").replace('\\', '/')
+
     path_to_yaml_files = os.environ["APP_CONF_DIR"]
+
     full_path = os.path.join(path_to_yaml_files, file_name)
 
     if full_path[-5:] == ".yaml":
